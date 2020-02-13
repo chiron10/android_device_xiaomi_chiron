14fb868b9caa72ce757401a8ed04cdbebd5a7697
andr68rus <ashtmb@gmail.com>
2020-02-13 10:08:21 +0300

    ROS-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_chiron.mk
+    $(LOCAL_DIR)/revengeos_chiron.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_chiron-user \
-    lineage_chiron-userdebug \
-    lineage_chiron-eng
+    revengeos_chiron-user \
+    revengeos_chiron-userdebug \
+    revengeos_chiron-eng
